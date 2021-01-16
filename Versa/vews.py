a8e922a4f210bc2a08a50a9d39714217e73d1f18
Dmitry Maleev <dim.maleev@live.com>
2021-01-16 18:31:02 +0300

    Finish dsta
    This code is perfect

--- Versa/vews.py
+++ Versa/vews.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+
 def reverse(request):
-    return render(request, 'reverse.html')
+    us_tx = request.GET['usertext']
+    rev_tx = us_tx[::-1]
+    return render(request, 'reverse.html', {'usertext':us_tx, 'reveresetx':rev_tx})
