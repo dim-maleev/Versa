9c1ba0e4f6c4799be465cffa2482ef4c6fc59abe
Dmitry Maleev <dim.maleev@live.com>
2021-01-16 18:47:35 +0300

    Final commit
    Add count

--- Versa/vews.py
+++ Versa/vews.py
@@ -7,5 +7,7 @@ def home(request):
 
 def reverse(request):
     us_tx = request.GET['usertext']
+    words = us_tx.split()
+    number_of = len(words)
     rev_tx = us_tx[::-1]
-    return render(request, 'reverse.html', {'usertext':us_tx, 'reveresetx':rev_tx})
+    return render(request, 'reverse.html', {'usertext':us_tx, 'reveresetx':rev_tx, 'numberwords':number_of})
